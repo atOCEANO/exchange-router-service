3d2d87d6aed4557fe146abc5998bb541d672a22d
DinisMartinho <111660063+DinisMartinho@users.noreply.github.com>
2026-04-25 16:32:39 +0100

    chore(release): bump version to 1.0.2

--- src/main.py
+++ src/main.py
@@ -14,7 +14,7 @@ logging.basicConfig(level=logging.INFO, format='%(levelname)s:     %(name)s - %(
 
 stream_manager = StreamManager()
 
-SERVICE_VERSION = "1.1.0"
+SERVICE_VERSION = "1.0.2"
 
 
 @asynccontextmanager
