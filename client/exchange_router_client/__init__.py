bc300dcff74c183f463b32c4fda958b1975d45d4
DinisMartinho <111660063+DinisMartinho@users.noreply.github.com>
2026-06-13 20:50:44 +0100

    sdk: export both clients, BatchResult, RouterDataWarning, the error tree, and the funding helpers

--- client/exchange_router_client/__init__.py
+++ client/exchange_router_client/__init__.py
@@ -1,3 +1,30 @@
 from .client import ExchangeRouterClient
+from .async_client import AsyncExchangeRouterClient
+from .batch import BatchResult
+from .funding import funding_paid, per_hour_view
+from ._warnings import RouterDataWarning
+from .errors import (
+    RouterError,
+    BadRequest,
+    NotFound,
+    RateLimited,
+    UpstreamUnavailable,
+    NotSupported,
+    RouterUnreachable,
+)
 
-__all__ = ["ExchangeRouterClient"]
+__all__ = [
+    "ExchangeRouterClient",
+    "AsyncExchangeRouterClient",
+    "BatchResult",
+    "funding_paid",
+    "per_hour_view",
+    "RouterDataWarning",
+    "RouterError",
+    "BadRequest",
+    "NotFound",
+    "RateLimited",
+    "UpstreamUnavailable",
+    "NotSupported",
+    "RouterUnreachable",
+]
